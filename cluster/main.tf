5134dc1d4df70f896004dee740471121ef717495
Priyanka <abhalepriyanka09@gmail.com>
2026-08-07 18:44:04 +0530

    Configure EKS module in main.tf
    Added EKS module configuration with node group settings.

--- cluster/main.tf
+++ cluster/main.tf
@@ -1,1 +1,23 @@
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
 
+name                  = var.cluster_name
+kubernetes_version = var.kubernetes_version
+
+subnet_ids = var.subnet_ids
+
+endpoint_public_access = true
+
+enable_cluster_creator_admin_permissions = true
+
+eks_managed_node_groups = {
+  worker_nodes = {
+    instance_types = [var.node_instance_type]
+
+    min_size      =2
+    max_size      =2
+    desired_size  =2
+    }
+   }
+}
