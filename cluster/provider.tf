69ed32bc722e12edf408ab8a779b15885b24b7a9
Priyanka <abhalepriyanka09@gmail.com>
2026-08-07 18:16:40 +0530

    Add Terraform configuration for AWS provider

--- cluster/provider.tf
+++ cluster/provider.tf
@@ -1,1 +1,14 @@
+terraform {
+   required_version = ">=1.5.0"
 
+   required_provider {
+      aws = {
+         source = "hashicorp/aws"
+         version = "~> 6.0"
+      }
+  }
+}
+
+provider "aws" {
+   region = var.aws_region
+}
