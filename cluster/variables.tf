7f29291b79a444561eb02ca45069763a53be2021
Priyanka <abhalepriyanka09@gmail.com>
2026-08-07 18:55:31 +0530

    Define variables for EKS cluster configuration
    Add variables for AWS region, cluster name, Kubernetes version, subnet IDs, and node instance type.

--- cluster/variables.tf
+++ cluster/variables.tf
@@ -1,1 +1,23 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
 
+variable "cluster_name" {
+  type    = string
+  default = "my-eks-cluster"
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = "1.33"
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.micro"
+}
